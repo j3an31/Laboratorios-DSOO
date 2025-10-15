6b577f742146063e72fe3da547f8e52a8e99a772
Gian Franco Apaza Quispe <gapazaqui@unsa.edu.pe>
2025-10-14 21:46:43 -0500

    Añadiendo clase GestionCitas

--- laboratorio05/SistemaDeCitas.java
+++ laboratorio05/SistemaDeCitas.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class SistemaDeCitas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("<<< SISTEMA DE REGISTRO - DOCTORES >>>");
         System.out.print("\n¿Cuantos doctores registrará?: ");
         int numDoctores = sc.nextInt();
@@ -14,7 +14,7 @@ public class SistemaDeCitas {
         System.out.println("\n--- REGISTRO COMPLETADO ---");
         Doctor.mostrarDoctoresRegistrados(arrayList_Doctores);
 
-        System.out.println("<<< SISTEMA DE REGISTRO - PACIENTES >>>");
+        System.out.println("\n<<< SISTEMA DE REGISTRO - PACIENTES >>>");
         System.out.print("\n¿Cuantos pacientes registrará?: ");
         int numPacientes = sc.nextInt();
 
@@ -22,7 +22,15 @@ public class SistemaDeCitas {
         Paciente.registrarPaciente(sc, arrayList_Pacientes, numPacientes);
         System.out.println("\n--- REGISTRO COMPLETADO ---");
         Paciente.mostrarPacientesRegistrados(arrayList_Pacientes);
+
+        System.out.println("\n<<< GESTIÓN DE CITAS MÉDICAS - REGISTRO >>>");
+        System.out.print("¿Cuantas citas registrará?: ");
+        int numCitas = sc.nextInt();
         
+        GestionCitas gestorCitas = new GestionCitas();
+        GestionCitas.registrarCitas(sc, gestorCitas, numCitas);
+        GestionCitas.gestionarEstadoCita(sc, gestorCitas);
+        GestionCitas.mostrarCitasRegistradas(gestorCitas);
 
     sc.close();
     }
